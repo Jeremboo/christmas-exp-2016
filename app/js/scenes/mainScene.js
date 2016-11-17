bda830051595b08aee1b8ead394eadaa24069a81
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-11-17 23:26:06 +0100

    :gem: add addMesh() methods

--- app/js/scenes/mainScene.js
+++ app/js/scenes/mainScene.js
@@ -4,13 +4,33 @@ import Sphere from '../components/sphere'
 export default class MainScene extends Scene {
   constructor() {
     super()
+    this.meshCount = 0;
+    this.meshListeners = [];
 
     this.sphere = new Sphere()
-    this.sphere.position.z = -100
-    this.add( this.sphere )
+    this.sphere.position.z = -100;
+
+    this.addMesh( this.sphere );
   }
 
   update() {
-    this.sphere.update()
+    let i = this.meshCount;
+    while (--i >= 0) {
+      this.meshListeners[i].update(this.meshListeners);
+    }
+  }
+
+  addMesh(mesh) {
+    this.add(mesh);
+    if (!mesh.update) return;
+    this.meshListeners.push(mesh);
+    this.meshCount++;
+  }
+
+  removeMesh(mesh) {
+    this.add(mesh);
+    if (!mesh.update) return;
+    this.meshListeners.push(mesh.update);
+    this.meshCount--;
   }
 }
