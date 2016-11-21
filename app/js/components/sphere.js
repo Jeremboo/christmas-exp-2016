0d3589cdd9365ee2eeed83bc2936a485385cf266
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-11-22 00:17:25 +0100

    :gem: add textures

--- app/js/components/sphere.js
+++ app/js/components/sphere.js
@@ -1,9 +1,15 @@
-import { Mesh, SphereGeometry, MeshBasicMaterial } from 'three'
+import { Mesh, SphereGeometry } from 'three'
+import ThreejsTextureTool from 'threejs-texture-tool';
+
 
 export default class Sphere extends Mesh {
   constructor() {
     const geometry = new SphereGeometry( 20, 30, 30 )
-    const material = new MeshBasicMaterial( { color: 0xecf0f1, wireframe: true } )
+    const textureTool = new ThreejsTextureTool();
+
+    const biomeTexture = textureTool.createImageTexture('assets/images/biome.jpg');
+    const noiseTexture = textureTool.createImageTexture('assets/images/noise.jpg');
+    const material = biomeTexture.material;
     super( geometry, material )
   }
 
