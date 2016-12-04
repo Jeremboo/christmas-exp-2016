fbcc9d3644bb38e186d8cc44a12ab75e0ed13b13
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-12-04 18:43:41 +0100

    :racehorse: :art: update positionning of christmas tree

--- app/js/components/christmasTree.js
+++ app/js/components/christmasTree.js
@@ -1,32 +1,42 @@
-import { SkinnedMesh, MeshBasicMaterial, Object3D, Euler, Vector3 } from 'three'
+import { SkinnedMesh, MeshBasicMaterial, Object3D, Euler, Vector3, ArrowHelper, Ray } from 'three'
 
 import props from '../core/props';
 import { toRadians, getRandomFloat, getRandomEuler } from '../core/utils';
 
 
-export default class ChristmasTree extends Object3D {
+export default class ChristmasTree extends SkinnedMesh {
   constructor() {
-    super();
     // https://www.youtube.com/watch?v=eEqB-eKcv7k
     const { geometry, material } = props.objects.get('christmasTree');
-    this.christmasTree = new SkinnedMesh( geometry, new MeshBasicMaterial({ color: 'red', skinning: true }) );
-    this.christmasTree.castShadow = true;
-    this.christmasTree.receiveShadow = true;
-    this.christmasTree.position.z = 100; // TODO position from earth
-    this.christmasTree.rotation.set( toRadians(90), 0, 0);
-    this.christmasTree.scale.multiplyScalar(getRandomFloat(1, 2));
+    super( geometry, new MeshBasicMaterial({ color: 'red', skinning: true }));
+    this.castShadow = true;
+    this.receiveShadow = true;
+    this.scale.multiplyScalar(getRandomFloat(1, 2));
 
-    this.rotation.copy(getRandomEuler());
-    this.add(this.christmasTree);
+    // Position on sphere
+    this.position.x = Math.random() * 2 - 1;
+    this.position.y = Math.random() * 2 - 1;
+    this.position.z = Math.random() * 2 - 1;
+    this.position.normalize();
+    this.position.multiplyScalar( 100 );
+
+    // Make onject perpendicular
+    const up = new Vector3(0, -1, 0);
+    const ray = new Ray(this.position);
+    ray.lookAt(new Vector3());
+    // // calculate normal to center of sphere
+    const axis = up.clone().cross(ray.direction).normalize();
+    const angle = Math.acos(ray.direction.clone().dot(up));
+    this.quaternion.setFromAxisAngle(axis, angle);
 
     this.oldWorldPosition = new Vector3();
   }
 
   update( ) {
-    const newWorldPosition = new Vector3().applyMatrix4( this.christmasTree.matrixWorld );
+    const newWorldPosition = new Vector3().applyMatrix4( this.matrixWorld );
     // TODO Matriceeeee
     const dist = this.oldWorldPosition.clone().sub(newWorldPosition).multiplyScalar(0.1);
-    console.log(dist);
+    // console.log(dist);
     this.oldWorldPosition.copy(newWorldPosition);
     // update skeleton
     // const time = Date.now() * 0.003;
@@ -35,8 +45,9 @@ export default class ChristmasTree extends Object3D {
     //   toRadians(0),
     //   toRadians(10 * Math.sin(time)),
     // );
-    for (let i = (this.christmasTree.skeleton.bones.length - 1); i > 0; i--) {
-      this.christmasTree.skeleton.bones[i].rotation.setFromVector3(dist);
+    for (let i = (this.skeleton.bones.length - 1); i > 0; i--) {
+      // http://answers.unity3d.com/questions/46770/rotate-a-vector3-direction.html
+      this.skeleton.bones[i].rotation.setFromVector3(dist);
     }
   }
 }
