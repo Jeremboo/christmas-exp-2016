0f611787caf5c12211c2f23c368ec773f7a714ea
SoufianeLasri <soufiane.lasri@gmail.com>
2016-12-07 00:03:54 +0100

    Prevent sprites from being hidden behind transparent meshes

--- app/js/components/star.js
+++ app/js/components/star.js
@@ -2,7 +2,7 @@ import { Sprite, SpriteMaterial } from 'three'
 
 export default class Star extends Sprite {
   constructor( texture ) {
-    const spriteMaterial = new SpriteMaterial( { map: texture } )
+    const spriteMaterial = new SpriteMaterial( { map: texture, depthTest: false } )
     super( spriteMaterial )
   }
 
